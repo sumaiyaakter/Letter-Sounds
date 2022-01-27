45fc72736f6334a635bb89d473887dbb6ecfd131
sumaiyaakter <sumaiya.akter.shampa@gmail.com>
2022-01-27 21:57:24 +0600

    added keypress listener, modified hover effects

--- app/js/script.js
+++ app/js/script.js
@@ -9,153 +9,169 @@ for (let i = 0; i < len; i++) {
     audioPlay(text);
 
     // add active class
-    var current = document.getElementsByClassName("active");
+    var current = document.getElementsByClassName('active');
     if (current.length > 0) {
-      current[0].className = current[0].className.replace(" active", "");
+      current[0].className = current[0].className.replace(' active', '');
     }
-    this.className += " active";
-  });
+    this.className += ' active';
 
+  });
 }
+
+// keypress
+document.addEventListener('keypress', function (event) {
+  var text = event.key;
+  audioPlay(text);
+  addClass(text);
+})
+
+// add active class
+function addClass(text) {
+  var selectButton = document.querySelector("." + text);
+  selectButton.classList.add('active');
+
+  setTimeout(function (){
+    selectButton.classList.remove('active');
+  }, 1000);
+};
 
 
 function audioPlay(text) {
   switch (text) {
-    case "a":
-      var audio = new Audio("./../audio/a.wav");
+    case 'a':
+      var audio = new Audio('./../audio/a.wav');
       audio.play();
       break;
 
-    case "b":
-      var audio = new Audio("audio/b.wav");
+    case 'b':
+      var audio = new Audio('audio/b.wav');
       audio.play();
       break;
 
-    case "c":
-      var audio = new Audio("audio/c.wav");
+    case 'c':
+      var audio = new Audio('audio/c.wav');
       audio.play();
       break;
 
-    case "d":
-      var audio = new Audio("audio/d.wav");
+    case 'd':
+      var audio = new Audio('audio/d.wav');
       audio.play();
       break;
 
-    case "e":
-      var audio = new Audio("audio/e.wav");
+    case 'e':
+      var audio = new Audio('audio/e.wav');
       audio.play();
       break;
 
-    case "f":
-      var audio = new Audio("audio/f.wav");
+    case 'f':
+      var audio = new Audio('audio/f.wav');
       audio.play();
       break;
 
-    case "g":
-      var audio = new Audio("audio/g.wav");
+    case 'g':
+      var audio = new Audio('audio/g.wav');
       audio.play();
       break;
 
-    case "h":
-      var audio = new Audio("audio/h.wav");
+    case 'h':
+      var audio = new Audio('audio/h.wav');
       audio.play();
       break;
 
-    case "i":
-      var audio = new Audio("audio/i.wav");
+    case 'i':
+      var audio = new Audio('audio/i.wav');
       audio.play();
       break;
 
-    case "j":
-      var audio = new Audio("audio/j.wav");
+    case 'j':
+      var audio = new Audio('audio/j.wav');
       audio.play();
       break;
 
-    case "k":
-      var audio = new Audio("audio/k.wav");
+    case 'k':
+      var audio = new Audio('audio/k.wav');
       audio.play();
       break;
 
-    case "l":
-      var audio = new Audio("audio/l.wav");
+    case 'l':
+      var audio = new Audio('audio/l.wav');
       audio.play();
       break;
 
-    case "m":
-      var audio = new Audio("audio/m.wav");
+    case 'm':
+      var audio = new Audio('audio/m.wav');
       audio.play();
       break;
 
-    case "m":
-      var audio = new Audio("audio/m.wav");
+    case 'm':
+      var audio = new Audio('audio/m.wav');
       audio.play();
       break;
 
-    case "n":
-      var audio = new Audio("audio/n.wav");
+    case 'n':
+      var audio = new Audio('audio/n.wav');
       audio.play();
       break;
 
-    case "o":
-      var audio = new Audio("audio/o.wav");
+    case 'o':
+      var audio = new Audio('audio/o.wav');
       audio.play();
       break;
 
-    case "p":
-      var audio = new Audio("audio/p.wav");
+    case 'p':
+      var audio = new Audio('audio/p.wav');
       audio.play();
       break;
 
-    case "q":
-      var audio = new Audio("audio/q.wav");
+    case 'q':
+      var audio = new Audio('audio/q.wav');
       audio.play();
       break;
 
-    case "r":
-      var audio = new Audio("audio/r.wav");
+    case 'r':
+      var audio = new Audio('audio/r.wav');
       audio.play();
       break;
 
-    case "s":
-      var audio = new Audio("audio/s.wav");
+    case 's':
+      var audio = new Audio('audio/s.wav');
       audio.play();
       break;
 
-    case "t":
-      var audio = new Audio("audio/t.wav");
+    case 't':
+      var audio = new Audio('audio/t.wav');
       audio.play();
       break;
 
-    case "u":
-      var audio = new Audio("audio/u.wav");
+    case 'u':
+      var audio = new Audio('audio/u.wav');
       audio.play();
       break;
 
-    case "v":
-      var audio = new Audio("audio/v.wav");
+    case 'v':
+      var audio = new Audio('audio/v.wav');
       audio.play();
       break;
 
-    case "w":
-      var audio = new Audio("audio/w.wav");
+    case 'w':
+      var audio = new Audio('audio/w.wav');
       audio.play();
       break;
 
-    case "x":
-      var audio = new Audio("audio/x.wav");
+    case 'x':
+      var audio = new Audio('audio/x.wav');
       audio.play();
       break;
 
-    case "y":
-      var audio = new Audio("audio/y.wav");
+    case 'y':
+      var audio = new Audio('audio/y.wav');
       audio.play();
       break;
 
-    case "z":
-      var audio = new Audio("audio/z.wav");
+    case 'z':
+      var audio = new Audio('audio/z.wav');
       audio.play();
       break;
   }
 
 }
-
